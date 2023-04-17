6255aa349e990681e6fd4060697f3251afc246a5
u-jin209 <o_o209@naver.com>
2023-04-17 14:51:23 +0900

    첫번째 커밋입니다

---
+++ src/main/java/com/inzent/injoy/InjoyApplication.java
@@ -0,0 +1,14 @@
+package com.inzent.injoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class InjoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjoyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inzent/injoy/InjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzent.injoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
