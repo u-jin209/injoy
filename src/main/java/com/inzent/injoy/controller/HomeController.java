0a581e3d16dff8bd1d4683c426b68094ad44ab6b
BTC-06 <cyg8937@gmail.com>
2023-04-20 17:34:36 +0900

    Merge remote-tracking branch 'origin/Cherry'3

--- src/main/java/com/inzent/injoy/controller/HomeController.java
+++ src/main/java/com/inzent/injoy/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.inzent.injoy.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String test() {
-        return "test";
+    public String test(Model model) {
+        model.addAttribute("tmp","이것은 컨트롤에서부터 왔습니다.");
+
+        return "cherry";
 
     }
 }
--- src/main/java/com/inzent/injoy/model/UserDTO.java
+++ src/main/java/com/inzent/injoy/model/UserDTO.java
@@ -8,9 +8,4 @@ public class UserDTO {
     private String username;
     private String password;
     private String role;
-
-    //이것을 추가했습니다.
-    private String Cherry;
-    private String Cherry2;
-
 }
