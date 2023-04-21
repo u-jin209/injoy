940240331ed6af3034c8df68bbeee46d15dd814f
gktnwls1127 <gktnwls1127@naver.com>
2023-04-21 17:49:43 +0900

    sujin - project task add

---
+++ src/main/resources/static/js/project/mainProject.js
@@ -0,0 +1,23 @@
+$(function (){
+    let hom_tab = '#home-tab';
+    let home_tabPane = '#home-tab-pane'
+    $(hom_tab).addClass('active')
+    $(home_tabPane).addClass('show active')
+
+    $('.mainNav[data-bs-toggle="tab"]').on('shown.bs.tab', function (e){
+        localStorage.setItem('selectedTab', $(e.target).attr('id'))
+        localStorage.setItem('selectedPane', $(e.target).attr('data-bs-target'))
+    })
+    let selectedTab = localStorage.getItem('selectedTab');
+    let selectedPane = localStorage.getItem('selectedPane');
+
+    if (selectedTab){
+
+        $(hom_tab).removeClass('active')
+        $(home_tabPane).removeClass('show active')
+        $('#'+selectedTab).addClass('active');
+        $(selectedPane).addClass('show active');
+
+
+    }
+})
