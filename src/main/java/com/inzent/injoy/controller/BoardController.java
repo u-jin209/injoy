e8c829e9f7c33b2ca03684f43110375555fb7014
gktnwls1127 <gktnwls1127@naver.com>
2023-04-25 09:22:10 +0900

    sujin -04/25 commit

---
+++ src/main/java/com/inzent/injoy/controller/BoardController.java
@@ -0,0 +1,40 @@
+package com.inzent.injoy.controller;
+
+import com.inzent.injoy.model.BoardDTO;
+import com.inzent.injoy.model.ProjectDTO;
+import com.inzent.injoy.model.UserCustomDetails;
+import com.inzent.injoy.service.BoardService;
+import com.inzent.injoy.service.ProjectService;
+import com.inzent.injoy.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    private UserService userService;
+    private BoardService boardService;
+
+    public BoardController(UserService userService, BoardService boardService) {
+        this.userService = userService;
+        this.boardService = boardService;
+    }
+
+    @PostMapping("write")
+    public String writeBoard(@AuthenticationPrincipal UserCustomDetails login, BoardDTO boardDTO){
+        boardDTO.setBoardWriterId(login.getUserDTO().getId());
+        boardDTO.setProjectId(1);
+
+        boardService.insert(boardDTO);
+        return "/project/projectHome";
+    }
+
+}
--- src/main/java/com/inzent/injoy/controller/HomeController.java
+++ src/main/java/com/inzent/injoy/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.inzent.injoy.controller;
+import com.inzent.injoy.model.BoardDTO;
+import com.inzent.injoy.service.BoardService;
+import com.inzent.injoy.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
+
 
 @Controller
 public class HomeController {
+    private UserService userService;
+    private BoardService boardService;
+
+    public HomeController(UserService userService, BoardService boardService) {
+        this.userService = userService;
+        this.boardService = boardService;
+    }
 
     @GetMapping("/")
     public String test() {
@@ -16,7 +28,9 @@ public class HomeController {
     }
 
     @GetMapping("/project")
-    public String showProject() {
+    public String showProject(Model model) {
+        List<BoardDTO> boardList = boardService.selectAll(1);
+        model.addAttribute("boardList", boardList);
         return "project/mainProject";
 
     }
