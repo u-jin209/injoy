e56bfdd1c10d3a0f7e034492c8599416b3ab79b1
u-jin209 <o_o209@naver.com>
2023-05-11 13:58:45 +0900

    0510 projectList ajax 변경 완료

--- src/main/resources/static/js/project/myProject.js
+++ src/main/resources/static/js/project/myProject.js
@@ -19,32 +19,32 @@ function projectList(){
                                 "<div class='project' style='padding: 10px 20px 10px 20px'>"+
                                     "<div class='row' >"+
                                         "<div class='col' style='text-align: left'>"+
-                                            "<button  class='btn' style='|${p.getBookMarkId() == 0  ? 'display:unset ;': 'display :none ;'}|' onclick='|bookMark(${p.getProjectId()}, ${p.getUserId()})|'>"+
+                                            "<button  class='btn' id='star"+item.projectId+"'  style='display: none'  onclick='bookMark("+ item.projectId +","+ item.userId+")'>"+
                                             "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#000' class='bi bi-star' viewBox='0 0 16 16'>"+
                                                 "<path d='M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z'/>"+
                                             "</svg></button>"+
-                                        "<button id='starFill"+item.projectId+"' class='btn' style='|${ p.getBookMarkId() == 0 ? 'display : none ;': 'display : unset ;'}|' onclick='|bookMark(${p.getProjectId()}, ${p.getUserId()})|'>"+
+                                        "<button id='starFill"+item.projectId+"' class='btn' style='display: none'  onclick='bookMark("+ item.projectId +","+ item.userId+")'>"+
                                         "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#FFB30D ' class='bi bi-star-fill' viewBox='0 0 16 16'>"+
                                             "<path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z'/>"+
                                         "</svg> </button> </div>"+
                                 "<div class='col' style='text-align: end'>"+
-                                    "<button class='btn ' type='button' id=  data-toggle='modal' data-target='#memberModal' onclick='memberList(this)'>" +
+                                    "<button class='btn ' type='button' id= "+item.projectId+"  data-toggle='modal' data-target='#memberModal' onclick='memberList(this)'>" +
                                         "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-people-fill' viewBox='0 0 16 16'>"+
                                             "<path d='M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z'/>"+
                                         "</svg></button> </div> </div>" +
 
-                        "<div class='row' id= >"+
+                        "<div class='row' id= "+item.projectId+" >"+
                             "<div class='col' style='float: left;'>" +
-                                "<div class='subTitle' >"+item.organName+"</div>"+
+                                "<div class='subTitle' id='organName"+item.projectId+"' >"+item.organName+"</div>"+
                             "</div></div>"+
 
 
-                       " <div class='row' style='display: block; MARGIN-RIGHT: 0;' onclick='goProject(this)'>"+
+                       " <div class='row' style='display: block; MARGIN-RIGHT: 0;' id= "+item.projectId+" onclick='goProject(this)'>"+
                             "<div class='col projectTitle' style='margin: 5px 10px 0px 10px; OVERFLOW: hidden; TEXT-OVERFLOW: ellipsis;' >"+item.projectName+"</div>"+
                             "<div class='col explanationText' > "+item.explanation+"</div>"+
                         "</div>"+
                     "</div>"+
-                        "<div class='modal fade' id='memberModal'  tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >"+
+                        "<div class='modal fade' id='memberModal' name = "+item.projectId+" tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >"+
 
                             "<div class='modal-dialog' role='document'>"+
                                 "<div class='modal-content'>"+
@@ -63,7 +63,7 @@ function projectList(){
                                             "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"+
                                                 "<span aria-hidden='true'>&times;</span>"+
                                             "</button></div></div>"+
-                                    "<div class='modal-body scrollDiv' id='modal-body'  style='height: 500px;overflow-y: scroll  '>"+
+                                    "<div class='modal-body scrollDiv' id='modal-body' value= "+item.projectId+"  name = "+item.projectId+" style='height: 500px;overflow-y: scroll  '>"+
                                         "<div class='row mt-5 mb-5' style='justify-content: center; '>"+
                                             "<div class='col-md-10' id='modalList'></div></div></div>"+
                                     "<div class='modal-footer'>"+
@@ -72,6 +72,22 @@ function projectList(){
                                     "</div> </div></div></div> </div>"
 
                         );
+                        console.log("item.organName :" + item.organName )
+                        if (item.bookMarkId == 0){
+                            const star = document.getElementById("star"+item.projectId)
+                            star.style.display = "unset"
+                        }else{
+                            const star = document.getElementById("starFill"+item.projectId)
+                            star.style.display = "unset"
+                        }
+
+                        if (item.organName ==null){
+
+                            const organName = document.getElementById("organName"+item.projectId)
+                            organName.innerText = ""
+                        }
+
+
                     });
 
                 })
@@ -133,6 +149,8 @@ function approve(value){
         data: data,
         type: "GET",
         success: function (result) {
+
+            projectList();
             inviteList();
         }
     })
@@ -141,6 +159,7 @@ function approve(value){
 }
 
 function inviteList(){
+    console.log("inviteLicst")
     $('#inviteListBody').empty();
     $.ajax({
         type: 'GET',
@@ -170,6 +189,8 @@ function inviteList(){
                             "</div>" +
                             "</div>"
                         );
+
+                        $("#inviteModalBtn").click();
                     });
 
                 })
