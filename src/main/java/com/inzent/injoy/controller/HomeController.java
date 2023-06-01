47f3fb38cbde628892e28d7b48e7a7f8a9016d40
u-jin209 <o_o209@naver.com>
2023-06-01 16:01:34 +0900

    0601 에러페이지

--- src/main/java/com/inzent/injoy/controller/HomeController.java
+++ src/main/java/com/inzent/injoy/controller/HomeController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
 import java.util.*;
