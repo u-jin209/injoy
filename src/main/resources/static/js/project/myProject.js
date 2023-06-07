cb1ab7ed1cf5644f8f19501de09a9c1162ce22c0
u-jin209 <o_o209@naver.com>
2023-06-07 10:22:20 +0900

    0607 오류 수정

--- src/main/resources/static/js/project/myProject.js
+++ src/main/resources/static/js/project/myProject.js
@@ -46,7 +46,7 @@ function projectList(){
                             "<div class='col explanationText' > "+item.explanation+"</div>"+
                         "</div>"+
                     "</div>"+
-                        "<div class='modal fade' id='memberModal' name = "+item.projectId+" tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >"+
+                        "<div class='modal `' id='memberModal' name = "+item.projectId+" tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >"+
 
                             "<div class='modal-dialog' role='document'>"+
                                 "<div class='modal-content'>"+
