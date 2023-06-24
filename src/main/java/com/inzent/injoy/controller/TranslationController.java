8debb321db6e8372dba65a67d243b23f7d1d9c38
BTC-06 <cyg8937@gmail.com>
2023-06-24 16:11:03 +0900

    6월 24일 google translate 기능 완료

--- src/main/java/com/inzent/injoy/controller/TranslationController.java
+++ src/main/java/com/inzent/injoy/controller/TranslationController.java
@@ -2,11 +2,21 @@ package com.inzent.injoy.controller;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 
 @RestController
 public class TranslationController {
@@ -27,5 +37,35 @@ public class TranslationController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to translate the text.");
         }
+    }
+
+
+
+    @PostMapping("/translateText")
+    public ResponseEntity<Map<String, Object>> translateText(HttpServletRequest request) {
+        Map<String,Object> map = new HashMap<>();
+        String rawText = request.getParameter("text");
+        System.out.println("rawText = " + rawText);
+
+        // Set your API key
+        String apiKey = "AIzaSyDgXh3i1wbaAO9vIWwICMORiey17ulb_bw";
+
+        // Initialize the translation client with the API key
+        Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey).build().getService();
+
+        // Translate text from English to Spanish
+        String text = rawText;
+//        Translation translation = translate.translate(text, Translate.TranslateOption.sourceLanguage("en"),
+//                Translate.TranslateOption.targetLanguage("ko"));
+        Translation translation = translate.translate(text, Translate.TranslateOption.sourceLanguage("ko"),
+                Translate.TranslateOption.targetLanguage("en"));
+
+//        System.out.println("Source text: " + translation.getOriginalText());
+        System.out.println("번역 Translated text: " + translation.getTranslatedText());
+
+        String resultText = translation.getTranslatedText();
+        map.put("resultText", resultText);
+
+        return ResponseEntity.ok(map);
     }
 }
